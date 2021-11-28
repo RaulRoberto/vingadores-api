61a9c4d1b3ff17ec3bc2035a3a6d4e103cdc676b
Raul Roberto Pereira Dias <raulhendrix@gmail.com>
2021-11-28 18:59:34 -0300

    Primeiro commit para testes

---
+++ src/main/kotlin/com/projeto/vingadores/vingadoresapi/VingadoresApiApplication.kt
@@ -0,0 +1,11 @@
+package com.projeto.vingadores.vingadoresapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VingadoresApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<VingadoresApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/projeto/vingadores/vingadoresapi/VingadoresApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.projeto.vingadores.vingadoresapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VingadoresApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
